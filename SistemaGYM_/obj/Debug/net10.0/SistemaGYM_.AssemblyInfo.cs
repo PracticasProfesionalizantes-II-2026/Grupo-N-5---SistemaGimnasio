72f1372fd9c8d9c71212f2faf2a364a7814736b6
ainaraparedes <ainaraparedes.escuela@gmail.com>
2026-08-06 18:08:42 -0300

    :)

--- SistemaGYM_/obj/Debug/net10.0/SistemaGYM_.AssemblyInfo.cs
+++ SistemaGYM_/obj/Debug/net10.0/SistemaGYM_.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaGYM_")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f090184ee7545630838455104f19e4c933facc17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b2c4e94090cc849a58ed6516329331c229454cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaGYM_")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaGYM_")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
