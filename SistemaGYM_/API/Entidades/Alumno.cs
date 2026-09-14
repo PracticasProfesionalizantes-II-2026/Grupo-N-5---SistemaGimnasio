804815de11162cc0c059d7a73416790931265bdd
ainaraparedes <ainaraparedes.escuela@gmail.com>
2026-09-14 20:23:26 -0300

    nueva version
    version actualizada del sistema 👍

--- SistemaGYM_/API/Entidades/Alumno.cs
+++
@@ -1,9 +0,0 @@
-namespace SistemaGYM.Entidades;
-    public class Alumno: Usuario
-    {
-        public bool EstaActivo { get; set; }    
-          public ICollection<AlumnoSuscripcion> AlumnoSuscripciones { get; set; } = new List<AlumnoSuscripcion>();
-          public ICollection<Pago> AlumnoPagos{ get; set; } = new List<Pago>();
-          public ICollection<ActividadAlumno> ActividadesAlumno { get; set; } = new List<ActividadAlumno>();
-
-    }
--- SistemaGYM_/API/Logica/DTO/AuthDTO.cs
+++
@@ -1,5 +0,0 @@
-namespace SistemaGYM.Logica.DTOs;
-
-public record LoginDto(string Email, string Contrasenia);
-
-public record LoginResultDto(int Id, string Nombre, string Apellido, string Rol);
